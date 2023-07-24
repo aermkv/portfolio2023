689c3ae5f09752e53a52e987c11557ef897e1b5b
aermkv <anton.ermkv@gmail.com>
2023-07-24 09:40:40 -0400

    from Typed to span on WorkNav

--- src/components/WorkNav.jsx
+++ src/components/WorkNav.jsx
@@ -24,32 +24,38 @@ const WorkNav = () => {
                     <span className='mt-0 text-4xl md:text-6xl lg:text-7xl font-medium text-gray-600 hover:cursor-default'>Anton Ermakov</span>
                     <CustomLink className='group -mt-8 hover:text-gray-600' to='../pages/DoodleLabs'>
                         <span className='text-4xl md:text-6xl lg:text-7xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300'>Doodle Labs</span>
-                        <Typed className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl' strings={['Creative coder and designer for an NFT platform']} typeSpeed={2} backSpeed={2} />
+                        {/* <Typed className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl' strings={['Creative coder and designer for an NFT platform']} typeSpeed={2} backSpeed={2} /> */}
+                        <span className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl'>Creative coder and designer for an NFT platform</span>
                     </CustomLink>
 
                     <CustomLink className='group -mt-8 hover:text-gray-600' to='../pages/BlackGlass'>
                         <span className='text-4xl md:text-6xl lg:text-7xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300'>Black Glass</span>
-                        <Typed className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl' strings={['Style guide and templates for a business consultancy']} typeSpeed={2} backSpeed={2} />
+                        {/* <Typed className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl' strings={['Style guide and templates for a business consultancy']} typeSpeed={2} backSpeed={2} /> */}
+                        <span className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl'>Style guide and templates for a business consultancy</span>
                     </CustomLink>
 
                     <CustomLink className='group -mt-8 hover:text-gray-600' to='../pages/CreativeBrands'>
                         <span className='text-4xl md:text-6xl lg:text-7xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300'>Creative Brands</span>
-                        <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Branding and packaging design for consumer goods']} typeSpeed={2} backSpeed={2} />
+                        {/* <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Branding and packaging design for consumer goods']} typeSpeed={2} backSpeed={2} /> */}
+                        <span className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl'>Branding and packaging design for consumer goods</span>
                     </CustomLink>
 
                     <CustomLink className='group -mt-8 hover:text-gray-600' to='../pages/Framexpert'>
                         <span className='text-4xl md:text-6xl lg:text-7xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300'>Framexpert</span>
-                        <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Iconography, UI components, branding for CAD software']} typeSpeed={2} backSpeed={2} />
+                        {/* <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Iconography, UI components, branding for CAD software']} typeSpeed={2} backSpeed={2} /> */}
+                        <span className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl'>Iconography, UI components, branding for CAD software</span>
                     </CustomLink>
 
                     <CustomLink className='group -mt-8 hover:text-gray-600' to='../pages/Warner'>
                         <span className='text-4xl md:text-6xl lg:text-7xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300'>Warner Music Canada</span>
-                        <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Album artwork and collateral for Modern Space']} typeSpeed={2} backSpeed={2} />
+                        {/* <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Album artwork and collateral for Modern Space']} typeSpeed={2} backSpeed={2} /> */}
+                        <span className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl'>Album artwork and collateral for Modern Space</span>
                     </CustomLink>
 
                     <CustomLink className='group -mt-8 hover:text-gray-600' to='../pages/Misc2'>
                         <span className='text-4xl md:text-6xl lg:text-7xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300'>Misc & Early Days</span>
-                        <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Packaging, posters, pitch decks, and more']} typeSpeed={2} backSpeed={2} />
+                        {/* <Typed className='px-1 py-1 hidden group-hover:block text-xl md:text-2xl lg:text-4xl' strings={['Packaging, posters, pitch decks, and more']} typeSpeed={2} backSpeed={2} /> */}
+                        <span className='px-1 py-1 hidden group-hover:block duration-300 text-xl md:text-2xl lg:text-4xl'>Packaging, posters, pitch decks, and more</span>
                     </CustomLink>
 
                     {/* <CustomLink className='-mt-8 hover:text-gray-600 text-4xl md:text-6xl lg:text-8xl ease-in-out tracking-tighter hover:tracking-wide hover:font-medium duration-300' to='../pages/BlackGlass'>BLACK GLASS</CustomLink>
